fc542596c9a1097665af73911ccc9958734ea102
Nikolas Earl <72423846+DJEarl01@users.noreply.github.com>
2022-06-25 13:41:35 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    long inputLong;
+    long outputCalculation;
+    cout << "Enter number to root" << endl;
+    cin >> inputLong;
+    
+    cout << "The square root of  " << inputLong << " is " << outputCalculation << endl;
+}
